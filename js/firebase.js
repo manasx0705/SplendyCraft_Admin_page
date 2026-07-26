0d29121ac234cd5b135f577a7caff79a8c3ee900
manasx0705 <bhaleraomanas45@gmail.com>
2026-07-26 17:54:01 +0530

    Refactor Firebase initialization and add auth
    Updated Firebase imports to use CDN links and added authentication.

--- js/firebase.js
+++ js/firebase.js
@@ -1,21 +1,16 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyAKVPApIecxUANu_ceuaoqXbn7bWhkEato",
   authDomain: "splendycraft-admin-page.firebaseapp.com",
   projectId: "splendycraft-admin-page",
   storageBucket: "splendycraft-admin-page.firebasestorage.app",
   messagingSenderId: "959234187077",
-  appId: "1:959234187077:web:95878444396f439248a82c",
-  measurementId: "G-EC73SF5SJN"
+  appId: "1:959234187077:web:95878444396f439248a82c"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
